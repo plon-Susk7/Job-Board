fbd40eb1c1958f51cefcfa1ba3ec2ac85bbaeffe
priyash7 <priyashshah4@gmail.com>
2024-11-14 17:35:53 +0530

    added clickable link to cards

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -1,18 +1,19 @@
 
 const ProjectCard = ({prompt}) => {
     return (
-        <div className='border border-gray-200 rounded-lg shadow flex flex-col p-4 h-full max-w-sm'>
-            <div className='flex'>
-                <img src={prompt.logo} alt="React Icon" className='w-8 h-8 mr-2' />
+        < a href={prompt.url} target="_blank" rel="noreferrer noopener" >
+            <div className='border border-gray-200 rounded-lg shadow flex flex-col p-4 h-full max-w-sm'>
+                <div className='flex'>
+                    <img src={prompt.logo} alt="React Icon" className='w-8 h-8 mr-2' />
+                </div>
+                <div>
+                    <p className='text-lg font-bold'>{prompt.name}</p>
+                    <p className='text-gray-600'>{prompt.description}</p>
+                    <p>{prompt.language}</p>
+                    <p>{prompt.stars}</p>
+                </div>
             </div>
-            <div>
-                <p className='text-lg font-bold'>{prompt.name}</p>
-                <p className='text-gray-600'>{prompt.description}</p>
-                <p>{prompt.url}</p>
-                <p>{prompt.language}</p>
-                <p>{prompt.stars}</p>
-            </div>
-        </div>
+        </a>
     );
 };
 
