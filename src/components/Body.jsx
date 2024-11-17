638c830a0be4fc3aafe05e0b243420b06f5a800a
priyash7 <priyashshah4@gmail.com>
2024-11-17 15:34:14 +0530

    added filter buttons

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,15 +1,35 @@
 import ProjectCard from './ProjectCard';
 import Data from './data';
+import { useState } from 'react';
 
 const Body = () => {
+    const [data, setData] = useState(Data);
+
+    const filterAll = () => {
+        setData(Data);
+    }
+
+    const filterGsoc = () => {
+        const filteredData = Data.filter((data) => data.type === 'GSOC');
+        setData(filteredData)
+    }
+
+    const filterYc = () => {
+        const filteredData = Data.filter((data) => data.type === 'YC');
+        setData(filteredData)
+    }
 
     return (
         <>
             <div className="mx-auto min-h-screen w-full max-w-screen-md mt-4">
-                <h1 className="text-lg font-bold mb-5">All Projects</h1>
+                <div className='flex gap-5'>
+                    <button onClick={filterAll} className="text-lg font-bold mb-5 border border-gray-200 px-2 rounded transition-all duration-300 hover:bg-gray-200">All</button>
+                    <button onClick={filterGsoc} className="text-lg font-bold mb-5 border border-gray-200 px-2 rounded transition-all duration-300 hover:bg-gray-200">GSOC</button>
+                    <button onClick={filterYc} className="text-lg font-bold mb-5 border border-gray-200 px-2 rounded transition-all duration-300 hover:bg-gray-200">YC</button>
+                </div>
                 <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-                    {Data.length > 0 ? (
-                        Data.map((data, index) => {
+                    {data.length > 0 ? (
+                        data.map((data, index) => {
                             return (
                                 <div key={index} className="transform transition-transform duration-300 hover:translate-y-[-4px] hover:shadow-lg">
                                     <ProjectCard prompt={data} />
--- src/components/data.jsx
+++ src/components/data.jsx
@@ -5,127 +5,145 @@ const Data =  [
         "description": "A crowdsourced list of the best open-source projects on the internet. Built with the Dub TypeScript SDK.",
         "url": "https://github.com/dubinc/oss-gallery",
         "stars": 1107,
-        "logo": "https://avatars.githubusercontent.com/u/153106492?v=4"
+        "logo": "https://avatars.githubusercontent.com/u/153106492?v=4",
+        "type": "GSOC"
     },
     {
         "name": "freeCodeCamp",
         "description": "freeCodeCamp.org's open-source codebase and curriculum. Learn to code for free.",
         "url": "https://github.com/freeCodeCamp/freeCodeCamp",
         "stars": 405496,
-        "logo": "https://avatars.githubusercontent.com/u/9892522?v=4"
+        "logo": "https://avatars.githubusercontent.com/u/9892522?v=4",
+        "type": "GSOC"
     },
     {
         "name": "react",
         "description": "The library for web and native user interfaces.",
         "url": "https://github.com/facebook/react",
         "stars": 229267,
-        "logo": "https://avatars.githubusercontent.com/u/69631?v=4"
+        "logo": "https://avatars.githubusercontent.com/u/69631?v=4",
+        "type": "YC"
     },
     {
         "name": "core",
         "description": "🖖 Vue.js is a progressive, incrementally-adoptable JavaScript framework for building UI on the web.",
         "url": "https://github.com/vuejs/core",
         "stars": 47669,
-        "logo": "https://avatars.githubusercontent.com/u/6128107?v=4"
+        "logo": "https://avatars.githubusercontent.com/u/6128107?v=4",
+        "type": "YC"
     },
     {
         "name": "go",
         "description": "The Go programming language",
         "url": "https://github.com/golang/go",
         "stars": 124069,
-        "logo": "https://avatars.githubusercontent.com/u/4314092?v=4"
+        "logo": "https://avatars.githubusercontent.com/u/4314092?v=4",
+        "type": "YC"
     },
     {
         "name": "node",
         "description": "Node.js JavaScript runtime ✨🐢🚀✨",
         "url": "https://github.com/nodejs/node",
         "stars": 107790,
-        "logo": "https://avatars.githubusercontent.com/u/9950313?v=4"
+        "logo": "https://avatars.githubusercontent.com/u/9950313?v=4",
+        "type": "YC"
     },
     {
         "name": "rust",
         "description": "Empowering everyone to build reliable and efficient software.",
         "url": "https://github.com/rust-lang/rust",
         "stars": 98561,
-        "logo": "https://avatars.githubusercontent.com/u/5430905?v=4"
+        "logo": "https://avatars.githubusercontent.com/u/5430905?v=4",
+        "type": "GSOC"
     },
     {
         "name": "neovim",
         "description": "Vim-fork focused on extensibility and usability",
         "url": "https://github.com/neovim/neovim",
         "stars": 83310,
-        "logo": "https://avatars.githubusercontent.com/u/6471485?v=4"
+        "logo": "https://avatars.githubusercontent.com/u/6471485?v=4",
+        "type": "YC"
     },
     {
         "name": "bitcoin",
         "description": "Bitcoin Core integration/staging tree",
         "url": "https://github.com/bitcoin/bitcoin",
         "stars": 79522,
-        "logo": "https://avatars.githubusercontent.com/u/528860?v=4"
+        "logo": "https://avatars.githubusercontent.com/u/528860?v=4",
+        "type": "GSOC"
     },
     {
         "name": "oss-gallery",
         "description": "A crowdsourced list of the best open-source projects on the internet. Built with the Dub TypeScript SDK.",
         "url": "https://github.com/dubinc/oss-gallery",
         "stars": 1107,
-        "logo": "https://avatars.githubusercontent.com/u/153106492?v=4"
+        "logo": "https://avatars.githubusercontent.com/u/153106492?v=4",
+        "type": "GSOC"
     },
     {
         "name": "freeCodeCamp",
         "description": "freeCodeCamp.org's open-source codebase and curriculum. Learn to code for free.",
         "url": "https://github.com/freeCodeCamp/freeCodeCamp",
         "stars": 405496,
-        "logo": "https://avatars.githubusercontent.com/u/9892522?v=4"
+        "logo": "https://avatars.githubusercontent.com/u/9892522?v=4",
+        "type": "GSOC"
     },
     {
         "name": "react",
         "description": "The library for web and native user interfaces.",
         "url": "https://github.com/facebook/react",
         "stars": 229267,
-        "logo": "https://avatars.githubusercontent.com/u/69631?v=4"
+        "logo": "https://avatars.githubusercontent.com/u/69631?v=4",
+        "type": "YC"
     },
     {
         "name": "core",
         "description": "🖖 Vue.js is a progressive, incrementally-adoptable JavaScript framework for building UI on the web.",
         "url": "https://github.com/vuejs/core",
         "stars": 47669,
-        "logo": "https://avatars.githubusercontent.com/u/6128107?v=4"
+        "logo": "https://avatars.githubusercontent.com/u/6128107?v=4",
+        "type": "YC"
     },
     {
         "name": "go",
         "description": "The Go programming language",
         "url": "https://github.com/golang/go",
         "stars": 124069,
-        "logo": "https://avatars.githubusercontent.com/u/4314092?v=4"
+        "logo": "https://avatars.githubusercontent.com/u/4314092?v=4",
+        "type": "YC"
     },
     {
         "name": "node",
         "description": "Node.js JavaScript runtime ✨🐢🚀✨",
         "url": "https://github.com/nodejs/node",
         "stars": 107790,
-        "logo": "https://avatars.githubusercontent.com/u/9950313?v=4"
+        "logo": "https://avatars.githubusercontent.com/u/9950313?v=4",
+        "type": "YC"
     },
     {
         "name": "rust",
         "description": "Empowering everyone to build reliable and efficient software.",
         "url": "https://github.com/rust-lang/rust",
         "stars": 98561,
-        "logo": "https://avatars.githubusercontent.com/u/5430905?v=4"
+        "logo": "https://avatars.githubusercontent.com/u/5430905?v=4",
+        "type": "GSOC"
     },
     {
         "name": "neovim",
         "description": "Vim-fork focused on extensibility and usability",
         "url": "https://github.com/neovim/neovim",
         "stars": 83310,
-        "logo": "https://avatars.githubusercontent.com/u/6471485?v=4"
+        "logo": "https://avatars.githubusercontent.com/u/6471485?v=4",
+        "type": "YC"
     },
     {
         "name": "bitcoin",
         "description": "Bitcoin Core integration/staging tree",
         "url": "https://github.com/bitcoin/bitcoin",
         "stars": 79522,
-        "logo": "https://avatars.githubusercontent.com/u/528860?v=4"
-    }
+        "logo": "https://avatars.githubusercontent.com/u/528860?v=4",
+        "type": "GSOC"
+    },
 ];
 
 export default Data;
