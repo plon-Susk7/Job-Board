8a15e29c2ac11876a75d9b7456762f9a4b6faf91
pradnyabhukan <bhukanpradnya5@gmail.com>
2024-11-22 11:36:33 -0600

    Fixes #23: added active button state

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -29,9 +29,16 @@ const Body = () => {
         <>
             <div className="mx-auto min-h-screen w-full max-w-screen-md mt-4">
                 <div className='flex gap-5'>
-                    <button onClick={filterAll} className="text-lg font-bold mb-5 border border-gray-200 px-2 rounded transition-all duration-300 hover:bg-gray-200 dark:hover:bg-gray-700">All</button>
-                    <button onClick={filterGsoc} className="text-lg font-bold mb-5 border border-gray-200 px-2 rounded transition-all duration-300 hover:bg-gray-200 dark:hover:bg-gray-700">GSOC</button>
-                    <button onClick={filterYc} className="text-lg font-bold mb-5 border border-gray-200 px-2 rounded transition-all duration-300 hover:bg-gray-200 dark:hover:bg-gray-700">YC</button>
+                    <button onClick={filterAll} className={`text-lg font-bold mb-5 border border-gray-200 px-2 rounded transition-all duration-300 
+                            ${activeButton === 'All' ? 'bg-gray-300 dark:bg-gray-700' : 'hover:bg-gray-200 dark:hover:bg-gray-700'}`}
+                    >All</button>
+                    <button onClick={filterGsoc} className={`text-lg font-bold mb-5 border border-gray-200 px-2 rounded transition-all duration-300 
+                            ${activeButton === 'GSOC' ? 'bg-gray-300 dark:bg-gray-700' : 'hover:bg-gray-200 dark:hover:bg-gray-700'}`}
+                    >GSOC</button>
+                    <button onClick={filterYc} 
+                    className={`text-lg font-bold mb-5 border border-gray-200 px-2 rounded transition-all duration-300 
+                            ${activeButton === 'YC' ? 'bg-gray-300 dark:bg-gray-700' : 'hover:bg-gray-200 dark:hover:bg-gray-700'}`}
+                    >YC</button>
                 </div>
                 <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
                     {data.length > 0 ? (
