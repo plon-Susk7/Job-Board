0db3853eff956a2d59569c0176fafd027163e058
priyash7 <priyashshah4@gmail.com>
2024-11-22 16:06:14 +0530

    added margin to footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
   return (
-    <div className="fixed bottom-0 left-0 w-full bg-gray-100 dark:backdrop-blur-md dark:bg-opacity-10 text-center p-2 text-sm text-gray-700 dark:text-white shadow-md">
+    <div className="mt-3 bottom-0 left-0 w-full bg-gray-100 dark:backdrop-blur-md dark:bg-opacity-10 text-center p-2 text-sm text-gray-700 dark:text-white shadow-md">
       An open source project made with ❤️
     </div>
   );
--- src/components/data.jsx
+++ src/components/data.jsx
@@ -4,7 +4,7 @@ const Data =  [
         "name": "kubeflow",
         "description": "Machine Learning Toolkit for Kubernetes",
         "url": "https://github.com/kubeflow/kubeflow",
-        "stars": 14401,
+        "stars": 14403,
         "logo": "https://avatars.githubusercontent.com/u/33164907?v=4",
         "type": "GSOC"
     },
@@ -12,7 +12,7 @@ const Data =  [
         "name": "keploy",
         "description": "Unit and Integration Test generation for Developers. Generate tests and stubs for your application that actually work!",
         "url": "https://github.com/keploy/keploy",
-        "stars": 5120,
+        "stars": 5121,
         "logo": "https://avatars.githubusercontent.com/u/92252339?v=4",
         "type": "GSOC"
     },
@@ -20,7 +20,7 @@ const Data =  [
         "name": "electron",
         "description": ":electron: Build cross-platform desktop apps with JavaScript, HTML, and CSS",
         "url": "https://github.com/electron/electron",
-        "stars": 114474,
+        "stars": 114494,
         "logo": "https://avatars.githubusercontent.com/u/13409222?v=4",
         "type": "GSOC"
     },
@@ -28,7 +28,7 @@ const Data =  [
         "name": "FFmpeg",
         "description": "Mirror of https://git.ffmpeg.org/ffmpeg.git",
         "url": "https://github.com/FFmpeg/FFmpeg",
-        "stars": 46146,
+        "stars": 46212,
         "logo": "https://avatars.githubusercontent.com/u/729418?v=4",
         "type": "GSOC"
     },
@@ -36,15 +36,95 @@ const Data =  [
         "name": "angular",
         "description": "Deliver web apps with confidence 🚀",
         "url": "https://github.com/angular/angular",
-        "stars": 96287,
+        "stars": 96297,
         "logo": "https://avatars.githubusercontent.com/u/139426?v=4",
+        "type": "GSOC"
+    },
+    {
+        "name": "processing",
+        "description": "Source code for the Processing Core and Development Environment (PDE)",
+        "url": "https://github.com/processing/processing",
+        "stars": 6482,
+        "logo": "https://avatars.githubusercontent.com/u/1617169?v=4",
+        "type": "GSOC"
+    },
+    {
+        "name": "incubator",
+        "description": "Apache Incubator Website",
+        "url": "https://github.com/apache/incubator",
+        "stars": 48,
+        "logo": "https://avatars.githubusercontent.com/u/47359?v=4",
+        "type": "GSOC"
+    },
+    {
+        "name": "mediawiki",
+        "description": "🌻 The collaborative editing software that runs Wikipedia. Mirror from https://gerrit.wikimedia.org/g/mediawiki/core. See https://mediawiki.org/wiki/Developer_access for contributing.",
+        "url": "https://github.com/wikimedia/mediawiki",
+        "stars": 4273,
+        "logo": "https://avatars.githubusercontent.com/u/56668?v=4",
+        "type": "GSOC"
+    },
+    {
+        "name": "Rocket.Chat",
+        "description": "The communications platform that puts data protection first.",
+        "url": "https://github.com/RocketChat/Rocket.Chat",
+        "stars": 40720,
+        "logo": "https://avatars.githubusercontent.com/u/12508788?v=4",
+        "type": "GSOC"
+    },
+    {
+        "name": "Bassa",
+        "description": "Automated Download Queue for Communities to take the best use of Internet bandwidth ",
+        "url": "https://github.com/scorelab/Bassa",
+        "stars": 168,
+        "logo": "https://avatars.githubusercontent.com/u/5285159?v=4",
+        "type": "GSOC"
+    },
+    {
+        "name": "RoboticsAcademy",
+        "description": "Learn Robotics with JdeRobot",
+        "url": "https://github.com/JdeRobot/RoboticsAcademy",
+        "stars": 317,
+        "logo": "https://avatars.githubusercontent.com/u/10959337?v=4",
+        "type": "GSOC"
+    },
+    {
+        "name": "mentorship-backend",
+        "description": "Mentorship System is an application that matches women in tech to mentor each other, on career development, through 1:1 relations during a certain period of time. This is the backend of this system.",
+        "url": "https://github.com/anitab-org/mentorship-backend",
+        "stars": 196,
+        "logo": "https://avatars.githubusercontent.com/u/47870548?v=4",
+        "type": "GSOC"
+    },
+    {
+        "name": "elasticsearch",
+        "description": "Free and Open Source, Distributed, RESTful Search Engine",
+        "url": "https://github.com/elastic/elasticsearch",
+        "stars": 1321,
+        "logo": "https://avatars.githubusercontent.com/u/6764390?v=4",
+        "type": "GSOC"
+    },
+    {
+        "name": "synapse",
+        "description": "Synapse: Matrix homeserver written in Python/Twisted.",
+        "url": "https://github.com/matrix-org/synapse",
+        "stars": 11829,
+        "logo": "https://avatars.githubusercontent.com/u/8418310?v=4",
+        "type": "GSOC"
+    },
+    {
+        "name": "bookbrainz-site",
+        "description": "BookBrainz website, written in node.js.",
+        "url": "https://github.com/metabrainz/bookbrainz-site",
+        "stars": 191,
+        "logo": "https://avatars.githubusercontent.com/u/293421?v=4",
         "type": "GSOC"
     },
     {
         "name": "danswer",
         "description": "Gen-AI Chat for Teams - Think ChatGPT if it had access to your team's unique knowledge.",
         "url": "https://github.com/danswer-ai/danswer",
-        "stars": 10688,
+        "stars": 10697,
         "logo": "https://avatars.githubusercontent.com/u/131946000?v=4",
         "type": "YC"
     },
@@ -52,7 +132,7 @@ const Data =  [
         "name": "stripe-python",
         "description": "Python library for the Stripe API.    ",
         "url": "https://github.com/stripe/stripe-python",
-        "stars": 1691,
+        "stars": 1692,
         "logo": "https://avatars.githubusercontent.com/u/856813?v=4",
         "type": "YC"
     },
@@ -60,16 +140,56 @@ const Data =  [
         "name": "unsloth",
         "description": "Finetune Llama 3.2, Mistral, Phi, Qwen 2.5 & Gemma LLMs 2-5x faster with 80% less memory",
         "url": "https://github.com/unslothai/unsloth",
-        "stars": 18338,
+        "stars": 18378,
         "logo": "https://avatars.githubusercontent.com/u/150920049?v=4",
         "type": "YC"
     },
     {
-        "name": "srcbook",
-        "description": "TypeScript-centric app development platform",
-        "url": "https://github.com/srcbookdev/srcbook",
-        "stars": 2564,
-        "logo": "https://avatars.githubusercontent.com/u/171983774?v=4",
+        "name": "supabase",
+        "description": "The open source Firebase alternative. Supabase gives you a dedicated Postgres database to build your web, mobile, and AI applications.",
+        "url": "https://github.com/supabase/supabase",
+        "stars": 73959,
+        "logo": "https://avatars.githubusercontent.com/u/54469796?v=4",
+        "type": "YC"
+    },
+    {
+        "name": "Font-Awesome",
+        "description": "The iconic SVG, font, and CSS toolkit",
+        "url": "https://github.com/FortAwesome/Font-Awesome",
+        "stars": 73922,
+        "logo": "https://avatars.githubusercontent.com/u/1505683?v=4",
+        "type": "YC"
+    },
+    {
+        "name": "mattermost",
+        "description": "Mattermost is an open source platform for secure collaboration across the entire software development lifecycle..",
+        "url": "https://github.com/mattermost/mattermost",
+        "stars": 30746,
+        "logo": "https://avatars.githubusercontent.com/u/9828093?v=4",
+        "type": "YC"
+    },
+    {
+        "name": "influxdb",
+        "description": "Scalable datastore for metrics, events, and real-time analytics",
+        "url": "https://github.com/influxdata/influxdb",
+        "stars": 29000,
+        "logo": "https://avatars.githubusercontent.com/u/5713248?v=4",
+        "type": "YC"
+    },
+    {
+        "name": "algoliasearch-client-javascript",
+        "description": "⚡️ A fully-featured and blazing-fast JavaScript API client to interact with Algolia.",
+        "url": "https://github.com/algolia/algoliasearch-client-javascript",
+        "stars": 1330,
+        "logo": "https://avatars.githubusercontent.com/u/2034458?v=4",
+        "type": "YC"
+    },
+    {
+        "name": "analytics.js",
+        "description": "The hassle-free way to integrate analytics into any web application.",
+        "url": "https://github.com/segmentio/analytics.js",
+        "stars": 4768,
+        "logo": "https://avatars.githubusercontent.com/u/819518?v=4",
         "type": "YC"
     }
 ];
